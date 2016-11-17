31a7647a671c2fe73dbbd56c598c404294313b7e
jackeraf <jarekimar@yahoo.es>
2016-11-18 00:22:03 +0100

    llegando al ajax

---
+++ file.js
@@ -0,0 +1,66 @@
+
+// song.track = function() {
+// 	var url = `https://api.spotify.com/v1/search?q=${this.query}&type=track&limit=12`
+// 	var req1 = $.get(url);
+// 	req1.done(GotFlow);
+// 	req1.fail(errorHandler);
+// };
+
+$(document).ready(function() {
+	
+
+	$(".form").submit(songMatcher);
+	
+
+	var searchSong = function(){
+		this.title;
+		this.artist;
+		this.cover;
+	}
+
+	console.log(searchSong)
+
+
+	searchSong.prototype.render=function (inputs){
+
+		console.log(this);
+		
+
+		var self = this;
+	
+	$.ajax({
+		type: "GET",
+		url: "https://api.spotify.com/v1/search?q="+ inputs +"&type=track&limit=12",
+		success: function (response){
+			console.log(response)
+			
+		},
+		error: function(error){
+			console.log(error + "error")
+		},
+	});
+}
+
+
+function songMatcher(){
+		console.log("visca songMatcher!")
+	
+		event.preventDefault();
+		var inputs = $("input").val();
+		console.log(inputs)
+		var newtrack = new searchSong(inputs);
+		console.log(newtrack)
+		newtrack.render();
+		clearSong ();
+
+
+	
+
+}
+
+function clearSong () {
+	$('form').empty();
+
+}
+
+});
